7c4ea22f99b909759bd01078ea6031e662918d52
robinhellen <robin.hellen@red-gate.com>
2015-06-02 15:57:58 +0100

    green

--- BankingKata/AccountLog.cs
+++ BankingKata/AccountLog.cs
@@ -1,28 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace BankingKata
 {
     public class AccountLog : IAccountLog
     {
-        private List<Money> log = new List<Money>();
+        private readonly List<ILogItem> log = new List<ILogItem>();
 
         public void RecordDeposit(Money amount)
         {
-            log.Add(amount);
+            log.Add(new DepositLogItem(amount));
         }
 
-        public void RecordWithdrawal(Money money)
+        public void RecordWithdrawal(Money amount)
         {
-            throw new NotImplementedException();
+            log.Add(new WithdrawalLogItem(amount));
         }
 
         public void CopyTo(IAccountLog other)
         {
-            foreach(var money in log)
+            foreach(var item in log)
             {
-                other.RecordDeposit(money);
+                item.RecordTo(other);
             }
+        }
+
+        private class WithdrawalLogItem : ILogItem
+        {
+            private readonly Money amount;
+
+            public WithdrawalLogItem(Money amount)
+            {
+                this.amount = amount;
+            }
+
+            public void RecordTo(IAccountLog destination)
+            {
+                destination.RecordWithdrawal(amount);
+            }
+        }
+
+        private class DepositLogItem : ILogItem
+        {
+            private readonly Money amount;
+
+            public DepositLogItem(Money amount)
+            {
+                this.amount = amount;
+            }
+
+            public void RecordTo(IAccountLog destination)
+            {
+                destination.RecordDeposit(amount);
+            }
+        }
+
+        private interface ILogItem {
+            void RecordTo(IAccountLog destination);
         }
     }
 }
