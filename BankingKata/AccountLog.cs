d58ccb6305ab40ebf6ec05ebd688b756fbb56e91
robinhellen <robin.hellen@red-gate.com>
2015-06-02 15:40:41 +0100

    basic test for this log

---
+++ BankingKata/AccountLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankingKata
+{
+    public class AccountLog : IAccountLog
+    {
+        private List<Money> log = new List<Money>();
+
+        public void RecordDeposit(Money amount)
+        {
+            log.Add(amount);
+        }
+
+        public void CopyTo(IAccountLog mock)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BankingKataTests/AccountLogTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingKata;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BankingKataTests
+{
+    class AccountLogTests
+    {
+        [Test]
+        public void LogStartsEmpty()
+        {
+            var accountLog = new AccountLog();
+
+            var mock = Substitute.For<IAccountLog>();
+            accountLog.CopyTo(mock);
+
+            mock.DidNotReceiveWithAnyArgs().RecordDeposit(null);
+        }
+    }
+}
